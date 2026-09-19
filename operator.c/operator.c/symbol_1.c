27a6a359dd8d357e76a70b0c3b847245fabb615b
silence886-pig <silence886-pig@users.noreply.github.com>
2026-09-19 16:42:55 +0800

    Initial commit: operator.c project

---
+++ operator.c/operator.c/symbol_1.c
@@ -0,0 +1,28 @@
+//在c语言中，%是取余的意思
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 3;
+//	int b = 2;
+//	printf("%d\n", a % b);
+//	return 0;
+//}
+//c语言中取余%必须要注意两个数都得是整数
+//#include<stdio.h>
+//int main()
+//{
+//	double a = 3.2;
+//	double b = 2.0;
+//	printf("%lf\n", a * b);
+//	//记住结果是double类型%后是lf
+//	return 0;
+//}
+//也可以直接进行数字运算
+//比如
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%f\n", 3.0 / 2.0);
+//	return 0;
+//}
+//tips：整数计算，结果一定是整数，浮点数计算，结果一定是浮点数，如果用了f那就整数也加.0
